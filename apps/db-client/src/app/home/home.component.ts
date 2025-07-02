2a424eb1599c44bc3135bc0d61283a19bd037926
Artur Kordzik <akordzik@outlook.com>
2025-07-02 22:37:04 +0100

    Added sign-in UI form

--- apps/db-client/src/app/home/home.component.ts
+++ apps/db-client/src/app/home/home.component.ts
@@ -1,8 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+  ],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private fb = inject(FormBuilder);
+
+  signInForm: FormGroup;
+  loading = false;
+
+  constructor() {
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.signInForm.valid) {
+      this.loading = true;
+      const formData = this.signInForm.value;
+
+      setTimeout(() => {
+        console.log('Sign in data:', formData);
+        this.loading = false;
+      }, 2000);
+    } else {
+      this.markFormGroupTouched();
+    }
+  }
+
+  private markFormGroupTouched() {
+    Object.keys(this.signInForm.controls).forEach((key) => {
+      const control = this.signInForm.get(key);
+      control?.markAsTouched();
+    });
+  }
+
+  get email() {
+    return this.signInForm.get('email');
+  }
+  get password() {
+    return this.signInForm.get('password');
+  }
+}
